e0c3e0b4b0d060e67b256d74f590adc7abbd9877
phanhuy <hn790078@gmail.com>
2026-01-12 10:39:38 +0700

    Added features include Link Preview, Infinite Scrolling, Multilingual support (i18n), and Giphy integration

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSignalR();
 builder.Services.AddEndpointsApiExplorer();
